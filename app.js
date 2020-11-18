d4b80f0db20e33f76a079750586eabab4e87f514
Stephen Beiter <stephenbeiter@gmail.com>
2020-11-18 14:14:59 -0800

    added player

---
+++ app.js
@@ -0,0 +1,10 @@
+const Player = require("./lib/Player");
+const Potion = require("./lib/Potion");
+
+var potion1 = new Potion();
+
+var player1 = new Player("Stamie");
+
+console.log(potion1);
+
+console.log(player1);
