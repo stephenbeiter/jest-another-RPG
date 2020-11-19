bd57cba11a8f7b41b59e5133a96b7ac60cf2910f
Stephen Beiter <stephenbeiter@gmail.com>
2020-11-18 22:50:57 -0800

    added enemy object

--- app.js
+++
@@ -1,8 +0,0 @@
-const Player = require("./lib/Player");
-const Potion = require("./lib/Potion");
-
-var potion1 = new Potion();
-
-var player1 = new Player("Stamie");
-
-console.log(player1.getAttackValue());
